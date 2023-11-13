a05519d1b769a5191bbe0156cb26df2cbce3b9d4
WhyK <windchimeyk@gmail.com>
2023-11-13 22:11:15 +0900

    chore: add biome

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig } from 'vite'
-import devServer from '@hono/vite-dev-server'
-import pages from '@hono/vite-cloudflare-pages'
+import { defineConfig } from "vite";
+import devServer from "@hono/vite-dev-server";
+import pages from "@hono/vite-cloudflare-pages";
 
 export default defineConfig({
   plugins: [
     pages(),
     devServer({
-      entry: 'src/index.tsx'
-    })
-  ]
-})
+      entry: "src/index.tsx",
+    }),
+  ],
+});
