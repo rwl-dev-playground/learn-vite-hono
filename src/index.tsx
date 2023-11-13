5a7e100939cd6f1eba14191805d2fdd2c9ba9dc1
WhyK <windchimeyk@gmail.com>
2023-11-13 22:12:21 +0900

    refactor: update UI

--- src/index.tsx
+++ src/index.tsx
@@ -6,7 +6,16 @@ const app = new Hono();
 app.get("*", renderer);
 
 app.get("/", (c) => {
-  return c.render(<h1>Hello!!!!</h1>);
+  return c.render(
+    <section>
+      <h2>Hello!!!!</h2>
+      <p />
+    </section>,
+    {
+      title: "Learn Vite Hono",
+      description: "HonoのViteテンプレートを学ぶ場所",
+    },
+  );
 });
 
 export default app;
--- src/renderer.tsx
+++ src/renderer.tsx
@@ -3,19 +3,34 @@ import { jsxRenderer } from "hono/jsx-renderer";
 
 declare module "hono" {
   interface ContextRenderer {
-    (content: string, props?: { title?: string }): Response;
+    (
+      content: string,
+      props?: { title?: string; description?: string },
+    ): Response;
   }
 }
 
 export const renderer = jsxRenderer(
-  ({ children, title }) => {
+  ({ children, title, description }) => {
     return (
       <html lang="ja">
         <head>
           <link href="/static/style.css" rel="stylesheet" />
           <title>{title}</title>
+          <meta name="description" content={description} />
         </head>
-        <body>{children}</body>
+        <body>
+          <header>
+            <h1>{title}</h1>
+            <span>{description}</span>
+          </header>
+          <main>{children}</main>
+          <footer>
+            <p>
+              <small>&copy; 2023-now WhyK</small>
+            </p>
+          </footer>
+        </body>
       </html>
     );
   },
